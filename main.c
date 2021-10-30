0982ff3fe145ae152a5040a589ed7cb8ceb0ff18
James Liu <james@jamesliu.io>
2021-10-30 18:11:18 +1100

    Read game version

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "helpers.h"
 
 uint8_t *memory;
 uint32_t memory_size;
+uint8_t zversion, zversion_specific;
 
 ZRet memory_read_byte(uint32_t addr, uint8_t *result) {
     if (addr > memory_size) {
@@ -62,15 +64,21 @@ int main(int argc, char *argv[]) {
     printf("Length of story file: %d\n", memory_size);
 
     uint16_t result;
-    ret = memory_read_word(0x00, &result);
-    if (ret != ZRet_Success) {
-        panic("Could not read word");
+    assert(memory_read_word(0x00, &result) == ZRet_Success);
+
+    assert(memory_write_byte(0x00, 0x08) == ZRet_Success);
+    assert(memory_write_word(0x01, 0x0200) == ZRet_Success);
+
+    assert(memory_read_byte(0x00, &zversion) == ZRet_Success);
+
+    zversion_specific = zversion;
+
+    if (zversion == 7 || zversion == 8) {
+        // 7 and 8 are similar to 5, and it's easier to handle less cases.
+        zversion = 5;
     }
 
-    printf("Word at 0x00: %#06hx", result);
-
-    memory_write_byte(0x00, 0x08);
-    memory_write_word(0x01, 0x0200);
+    printf("Version: %d Version specific: %d\n", zversion, zversion_specific);
 
     return EXIT_SUCCESS;
 }
